80699c2b4cf9d148e3c97329c69d83bfd1c4ed66
Michael Dally <mddally@gmail.com>
2022-05-17 13:58:43 -0400

    solved

--- lib/first_spicy_batch.rb
+++ lib/first_spicy_batch.rb
@@ -3,6 +3,22 @@
 # @return {boolean} whether the batch is spicy
 # def is_spicy_batch(batch):
 
+
 def first_spicy_batch(n)
-  # your code here
+  start = 0;
+  last = n;
+  while(last - start >= 0) do
+    half = (start + last) / 2
+    puts is_spicy_batch(half)
+    puts "middle = #{half}"
+    if is_spicy_batch(half)
+      last = half - 1
+      puts "last equals middle #{last}"
+    else
+      start = half + 1
+      puts "start equals middle #{start}"
+    end
+  end
+  puts "Last and start when we leave the while loop : #{last} + #{start}"
+  start
 end
