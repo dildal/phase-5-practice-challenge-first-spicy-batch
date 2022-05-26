0f8d073de2c24fc183b39e4e4a390dffe4e83010
Michael Dally <mddally@gmail.com>
2022-05-26 13:27:05 -0400

    commit tree e583a3af0822eeb1d0da50d758ef0132f39e5006

--- lib/first_spicy_batch.rb
+++ lib/first_spicy_batch.rb
@@ -5,20 +5,25 @@
 
 
 def first_spicy_batch(n)
-  start = 0;
-  last = n;
-  while(last - start >= 0) do
-    half = (start + last) / 2
-    puts is_spicy_batch(half)
-    puts "middle = #{half}"
-    if is_spicy_batch(half)
-      last = half - 1
-      puts "last equals middle #{last}"
-    else
-      start = half + 1
-      puts "start equals middle #{start}"
+  # want to find first spicy batch
+  # check if middle batch is spicy
+    # if it is that means the first half of the array contains first spicy batch
+      #set end to middle and check middle of that array
+    # else second half of array contains spicy batch
+      # set first to middle + 1
+      # check middle of that
+    # we know were at the first spicy batch when start = end
+    start = 0
+    last = n
+    mid = n/2
+    while(start < last) do
+      if(is_spicy_batch(mid))
+        last = mid;
+        mid = (start + last) / 2
+      else
+        start = mid + 1
+        mid = (start + last) / 2
+      end
     end
-  end
-  puts "Last and start when we leave the while loop : #{last} + #{start}"
-  start
+    start
 end
